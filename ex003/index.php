88a69a5d0e52a7dd7f25f5854fd051a90817e610
efss7 <ericfelipess07@gmail.com>
2023-02-16 23:22:21 -0300

    Tipos primitivos do PHP

---
+++ ex003/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em PHP</title>
+</head>
+
+<body>
+    <?php 
+        // 0x = hexadecimal 0b = binário 0 = octal 
+        // $num = 010;
+        // echo "O valor da variável é $num"
+
+        // $v = "Eric";
+        // var_dump($v);
+
+        //$num = (int) 3e2; //3 x 10^2
+        // echo "O valor é $num";
+        //var_dump($num)
+
+        // $num = (float) "950";
+        // var_dump($num);
+
+        // $casado = true;
+        // var_dump($casado);
+        // print "O valor para casado é $casado";
+
+        // $vet = [3, 5.3, "João", 6, true];
+        // var_dump($vet)
+
+        // class Pessoa{
+        //     private string $nome;
+        // }
+        
+        // $p = new Pessoa;
+        // var_dump($p);
+    ?>
+</body>
+
+</html>
